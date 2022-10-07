9972365ad7e71ed67a9103ff8ebbc25bbece6d30
Mahir Molhtar <mahr.bwb@gmail.com>
2022-10-07 03:46:54 +0300

    create and Input Field

--- lib/ui/widgets/input_field.dart
+++ lib/ui/widgets/input_field.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo/ui/size_config.dart';
+
+import '../theme.dart';
 
 class InputField extends StatelessWidget {
-  const InputField({Key? key}) : super(key: key);
+  const InputField({
+    Key? key,
+    required this.title,
+    required this.hint,
+    this.controller,
+    this.widget,
+  }) : super(key: key);
+  final String title, hint;
+  final TextEditingController? controller;
+
+  final Widget? widget;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left:8.0),
+          child: Text(
+            title,
+            style: titleStyle,
+          ),
+        ),
+        Container(
+          width: SizeConfig.screenWidth,
+          // height: 60,
+          padding: const EdgeInsets.only(top: 8.0),
+          margin: const EdgeInsets.only(left: 14.0, right: 15, top: 8.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              //color: primaryClr,
+              border: Border.all(color: Colors.grey)),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: controller,
+                  readOnly: widget != null ? true : false,
+                  cursorColor:
+                      Get.isDarkMode ? Colors.grey[100] : Colors.grey[700],
+                  autofocus: false,
+                  style: subTitleStyle,
+                  decoration: InputDecoration(
+                    hintText: hint,
+                    hintStyle: subTitleStyle,
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: context.theme.backgroundColor,
+                        width: 0,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: context.theme.backgroundColor,
+                        width: 0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              widget ?? Container(),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
