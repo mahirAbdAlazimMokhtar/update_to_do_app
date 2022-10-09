58b0a2d09a910f1c3836bd5e50bc04c5d06dc8ab
Mahir Molhtar <mahr.bwb@gmail.com>
2022-10-09 10:29:32 +0300

    edit padding and margin

--- lib/ui/widgets/input_field.dart
+++ lib/ui/widgets/input_field.dart
@@ -19,58 +19,61 @@ class InputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left:8.0),
-          child: Text(
-            title,
-            style: titleStyle,
+    return Container(
+      margin: const EdgeInsets.only(top:16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left:8.0),
+            child: Text(
+              title,
+              style: titleStyle,
+            ),
           ),
-        ),
-        Container(
-          width: SizeConfig.screenWidth,
-          // height: 60,
-          padding: const EdgeInsets.only(top: 8.0),
-          margin: const EdgeInsets.only(left: 14.0, right: 15, top: 8.0),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              //color: primaryClr,
-              border: Border.all(color: Colors.grey)),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  controller: controller,
-                  readOnly: widget != null ? true : false,
-                  cursorColor:
-                      Get.isDarkMode ? Colors.grey[100] : Colors.grey[700],
-                  autofocus: false,
-                  style: subTitleStyle,
-                  decoration: InputDecoration(
-                    hintText: hint,
-                    hintStyle: subTitleStyle,
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: context.theme.backgroundColor,
-                        width: 0,
+          Container(
+            width: SizeConfig.screenWidth,
+            // height: 60,
+            padding: const EdgeInsets.only(left: 14.0),
+            margin: const EdgeInsets.only(top: 8 ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                //color: primaryClr,
+                border: Border.all(color: Colors.grey)),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: controller,
+                    readOnly: widget != null ? true : false,
+                    cursorColor:
+                        Get.isDarkMode ? Colors.grey[100] : Colors.grey[700],
+                    autofocus: false,
+                    style: subTitleStyle,
+                    decoration: InputDecoration(
+                      hintText: hint,
+                      hintStyle: subTitleStyle,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: context.theme.backgroundColor,
+                          width: 0,
+                        ),
                       ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: context.theme.backgroundColor,
-                        width: 0,
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: context.theme.backgroundColor,
+                          width: 0,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              widget ?? Container(),
-            ],
+                widget ?? Container(),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
