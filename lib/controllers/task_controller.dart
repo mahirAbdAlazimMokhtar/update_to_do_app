702e8f6035ff8e24d40ee7084599e4e586a22880
Mahir Molhtar <mahr.bwb@gmail.com>
2022-10-09 17:11:09 +0300

    add taskList

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,1 +1,7 @@
-class TaskController {}
+import 'package:get/get.dart';
+import 'package:todo/models/task.dart';
+
+class TaskController extends GetxController{
+  List taskList = <Task>[];
+  getTask(){}
+}
