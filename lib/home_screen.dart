596a5fb750cfc9fc1feea51e55bfdfe387fd0503
Mahir Molhtar <mahr.bwb@gmail.com>
2022-10-09 10:28:49 +0300

    edit background cp;pr

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo/services/theme_services.dart';
+import 'package:todo/ui/pages/add_task_page.dart';
+import 'package:todo/ui/pages/notification_screen.dart';
+import 'package:todo/ui/size_config.dart';
+import 'package:todo/ui/theme.dart';
+import 'package:todo/ui/widgets/button.dart';
+import 'package:todo/ui/widgets/input_field.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
+      appBar: buildAppBar(),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              MyButton(
+                  label: 'Add Task',
+                  onTap: () {
+                    Get.to(() => const AddTaskPage());
+                  }),
+              const InputField(
+                title: 'Title',
+                hint: '',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  AppBar buildAppBar() => AppBar(
+        leading: IconButton(
+          onPressed: () => ThemeServices().switchTheme(),
+          icon: Icon(
+            Get.isDarkMode
+                ? Icons.wb_sunny_outlined
+                : Icons.nightlight_outlined,
+            size: 24,
+            color: Get.isDarkMode ? Colors.white : darkGreyClr,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: context.theme.backgroundColor,
+        actions: const [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/person.jpeg'),
+            radius: 18,
+          ),
+          SizedBox(
+            width: 18,
+          )
+        ],
+      );
 }
--- lib/ui/pages/notification_screen.dart
+++ lib/ui/pages/notification_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/ui/theme.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key, required this.payload}) : super(key: key);
-final String payload;
+  final String payload;
 
   @override
   _NotificationScreenState createState() => _NotificationScreenState();
@@ -11,6 +12,7 @@ final String payload;
 
 class _NotificationScreenState extends State<NotificationScreen> {
   String _payload = '';
+
   @override
   void initState() {
     super.initState();
@@ -20,19 +22,164 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
       appBar: AppBar(
-        title: Text(
-          _payload.toString().split('|')[0]
-        ),
+        title: Text(_payload.toString().split('|')[0]),
         leading: IconButton(
-          onPressed: ()=>Get.back(),
-          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Get.isDarkMode ? Colors.white : Colors.black,
+          ),
         ),
         elevation: 0,
         backgroundColor: context.theme.backgroundColor,
       ),
-      body: Container(
-
+      body: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Hello, Mahir',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w900,
+                        color: Get.isDarkMode ? Colors.white : darkGreyClr),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'You have a new Reminder',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        color: Get.isDarkMode ? Colors.grey[100] : darkGreyClr),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0, vertical: 10.0),
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: primaryClr),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //title
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.text_format,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              'Title',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 24),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          'Title',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        //description
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.description,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              'Description',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 24),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          'Description',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        // date
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.calendar_month,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              'Date',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 24),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          'Date',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        //
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
